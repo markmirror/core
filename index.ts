042c16552bc27b8a0fa20c8080e22e711cdd50fe
Hsiaoming Yang <me@lepture.com>
2022-06-04 00:28:19 +0900

    Design package structure for distribution

---
+++ index.ts
@@ -0,0 +1,15 @@
+export { MarkMirror } from "./src/editor"
+export { codeLanguages } from "./src/markdown"
+export {
+  onSelectionSet,
+  getSelectedNode,
+  getSelectedNodes,
+} from "./src/cursor"
+export { markdownKeymap } from "./src/commands"
+export * from "./src/commands/heading"
+export * from "./src/commands/blockquote"
+export * from "./src/commands/codeblock"
+export * from "./src/commands/lists"
+export * from "./src/commands/markers"
+export * from "./src/commands/links"
+export * from "./src/commands/extra"
--- src/markdown.ts
+++ src/markdown.ts
@@ -1,5 +1,7 @@
 import { LanguageDescription } from "@codemirror/language"
 import { html } from "@codemirror/lang-html"
+import { css } from "@codemirror/lang-css"
+import { javascript } from "@codemirror/lang-javascript"
 export { markdownLanguage } from "@codemirror/lang-markdown"
 
 
@@ -9,5 +11,37 @@ export const codeLanguages = [
     load: async () => {
       return html()
     }
-  })
+  }),
+  LanguageDescription.of({
+    name: "css",
+    load: async () => {
+      return css()
+    }
+  }),
+  LanguageDescription.of({
+    name: "javascript",
+    alias: ["js", "javascript"],
+    load: async () => {
+      return javascript()
+    }
+  }),
+  LanguageDescription.of({
+    name: "typescript",
+    alias: ["ts", "typescript"],
+    load: async () => {
+      return javascript()
+    }
+  }),
+  LanguageDescription.of({
+    name: "jsx",
+    load: async () => {
+      return javascript({ jsx: true })
+    }
+  }),
+  LanguageDescription.of({
+    name: "tsx",
+    load: async () => {
+      return javascript({ jsx: true, typescript: true })
+    }
+  }),
 ]
